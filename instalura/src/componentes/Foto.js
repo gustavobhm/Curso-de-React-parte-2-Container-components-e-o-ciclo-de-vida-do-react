a9677b3757bbdeb94d3b961164be914756558f19
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-08-22 11:49:53 -0300

    Aula 5 - Presentational e Container components

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class FotoAtualizacoes extends Component {
+
+  like(event) {
+    event.preventDefault();
+    this.props.like(this.props.foto.id);
+  }
+
+  comenta(event) {
+    event.preventDefault();
+    this.props.comenta(this.props.foto.id, this.comentario.value);
+  }
+
   render() {
     return (
 
       <section className="fotoAtualizacoes">
 
-        <a href="/" className="fotoAtualizacoes-like">Likar</a>
+        <a onClick={this.like.bind(this)} className={this.props.foto.likeada ? "fotoAtualizacoes-like-ativo" : "fotoAtualizacoes-like"}>Linkar</a>
 
-        <form className="fotoAtualizacoes-form">
-          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+        <form className="fotoAtualizacoes-form" onSubmit={this.comenta.bind(this)}>
+          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comentario = input} />
           <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
         </form>
 
@@ -21,10 +32,11 @@ class FotoAtualizacoes extends Component {
 }
 
 class FotoInfo extends Component {
+
   render() {
     return (
 
-      <div className="foto-in fo">
+      <div className="foto-info">
 
         <div className="foto-info-likes">
           {
@@ -60,6 +72,7 @@ class FotoInfo extends Component {
 }
 
 class FotoHeader extends Component {
+
   render() {
     return (
 
@@ -80,9 +93,11 @@ class FotoHeader extends Component {
 
     );
   }
+
 }
 
 export default class FotoItem extends Component {
+
   render() {
     return (
 
@@ -90,9 +105,10 @@ export default class FotoItem extends Component {
         <FotoHeader foto={this.props.foto} />
         <img alt="foto" className="foto-src" src={this.props.foto.urlFoto} />
         <FotoInfo foto={this.props.foto} />
-        <FotoAtualizacoes />
+        <FotoAtualizacoes {...this.props} />
       </div>
 
     );
   }
+
 }
